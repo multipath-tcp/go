81c741310881b8ed377359b0d1bd070f2b767c87
Gregory Detal <gregory.detal@tessares.net>
2023-01-18 17:46:41 +0100

    net: mptcp: add MultipathTCP checker
    This returns whether the connection is using MPTCP.
    
    From kernel version 5.16, it is possible to use:
    
        getsockopt(SOL_MPTCP,MPTCP_INFO)
    
    and check if EOPNOTSUPP (IPv4) or ENOPROTOOPT (IPv6) is returned. If it
    is, it means a fallback to TCP has been done. See this link for more
    details:
    
        https://github.com/multipath-tcp/mptcp_net-next/issues/294
    
    Before v5.16, there is no other simple way, from the userspace, to check
    if the created socket did a fallback to TCP. Netlink requests could be
    done to try to find more details about a specific socket but that seems
    quite a heavy machinery. Instead, only the protocol is checked on older
    kernels.
    
    Link: https://github.com/golang/go/issues/56539
    Signed-off-by: Gregory Detal <gregory.detal@tessares.net>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- src/net/mptcpsock_linux.go
+++ src/net/mptcpsock_linux.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"errors"
 	"internal/poll"
+	"internal/syscall/unix"
 	"sync"
 	"syscall"
 )
@@ -15,11 +16,14 @@ import (
 var (
 	mptcpOnce      sync.Once
 	mptcpAvailable bool
+	hasSOLMPTCP    bool
 )
 
 // These constants aren't in the syscall package, which is frozen
 const (
 	_IPPROTO_MPTCP = 0x106
+	_SOL_MPTCP     = 0x11c
+	_MPTCP_INFO    = 0x1
 )
 
 func supportsMultipathTCP() bool {
@@ -38,6 +42,10 @@ func initMPTCPavailable() {
 	default:
 		mptcpAvailable = true
 	}
+
+	major, minor := unix.KernelVersion()
+	// SOL_MPTCP only supported from kernel 5.16
+	hasSOLMPTCP = major > 5 || (major == 5 && minor >= 16)
 }
 
 func (sd *sysDialer) dialMPTCP(ctx context.Context, laddr, raddr *TCPAddr) (*TCPConn, error) {
@@ -72,4 +80,39 @@ func (sl *sysListener) listenMPTCP(ctx context.Context, laddr *TCPAddr) (*TCPLis
 	// But just in case MPTCP is blocked differently (SELinux, etc.), just
 	// retry with "plain" TCP.
 	return sl.listenTCP(ctx, laddr)
+}
+
+// Kernel >= 5.16 with SOL_MPTCP support
+func useMultipathTCPNew(fd *netFD) bool {
+	_, err := fd.pfd.GetsockoptInt(_SOL_MPTCP, _MPTCP_INFO)
+
+	// Error is not the expected one for fallback? MPTCP is then used
+	switch fd.family {
+	case syscall.AF_INET:
+		return err != syscall.EOPNOTSUPP
+	case syscall.AF_INET6:
+		return err != syscall.ENOPROTOOPT
+	}
+
+	// Should not happen
+	return false
+}
+
+// Kernel < 5.16 without SOL_MPTCP support
+func useMultipathTCPOld(fd *netFD) bool {
+	// Less good: only check the protocol being used but not fallback to TCP
+	proto, _ := fd.pfd.GetsockoptInt(syscall.SOL_SOCKET, syscall.SO_PROTOCOL)
+
+	return proto == _IPPROTO_MPTCP
+}
+
+// Check if MPTCP is being used
+func useMultipathTCP(fd *netFD) bool {
+	// Kernel >= 5.16 returns EOPNOTSUPP/ENOPROTOOPT in case of fallback.
+	// Older kernels will always return them: not usable
+	if hasSOLMPTCP {
+		return useMultipathTCPNew(fd)
+	}
+
+	return useMultipathTCPOld(fd)
 }
